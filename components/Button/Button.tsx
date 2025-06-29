6b675c35f2f8d2d797348ff4d43b17ad824d9a07
Mehdi Pouladgar <m.pouladgar@rahito.com>
2025-06-30 01:20:54 +0330

    change button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,10 @@
 interface Props {
   children: string;
+  type?: "primary" | "secondary";
 }
 
 function Button(props: Props) {
-  const { children } = props;
+  const { children, type } = props;
 
   return (
     <div className="bg-white rounded shadow-md text-[#5F94D3] font-bold px-2 py-3 text-center">
